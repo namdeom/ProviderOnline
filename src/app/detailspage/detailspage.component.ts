3032ad1a56cf13d5aa7e9c3a864d53a555de10ba
namdeom <manmohan.namdeo@gmail.com>
2018-12-22 22:26:01 -0500

    New Commit

--- src/app/detailspage/detailspage.component.ts
+++ src/app/detailspage/detailspage.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-detailspage',
     templateUrl: './detailspage.component.html',
-    //styleUrls: ['./detailspage.component.css']
+    ///styleUrls: ['./detailspage.component.css']
 })
 export class detailspagecomponent implements OnInit {
     constructor() { }
